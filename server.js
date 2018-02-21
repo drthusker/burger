75ba742e989885db869eed18035bc5b0b2664743
David Thompson <drthusker@gmail.com>
2018-02-20 21:38:50 -0700

    try again

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override')
 var app = express();
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 
 var db = process.env.DATABASE_URL || 'localhost'
 
