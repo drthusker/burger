67a08f2e0f00579725106feaa1790bb8d61298ae
David Thompson <drthusker@gmail.com>
2018-02-20 18:49:29 -0700

    heroku

--- db/schema.sql
+++ db/schema.sql
@@ -2,10 +2,11 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
-CREATE TABLE burgers(
-  id INTEGER AUTO_INCREMENT NOT NULL,
-  burger_name VARCHAR(50) NOT NULL,
-  devoured BOOLEAN NOT NULL DEFAULT 0,
-  PRIMARY KEY(id)
-);
-
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id)
+); 
